d34a0b6f41a51fb05d1d0ea3181abdcd51e9133f
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-09-25 01:27:40 +0000

    Updated pro card

--- app/src/routes/pros/+page.svelte
+++ app/src/routes/pros/+page.svelte
@@ -2,45 +2,45 @@
 	import { onMount } from 'svelte';
 	import ProCard from '$lib/components/ProCard.svelte';
 	import TeamCard from '$lib/components/TeamCard.svelte';
-	import { supabase } from '../../supabaseClient'; // Assuming you have a Supabase client setup
+	import { supabase } from '../../supabaseClient';
 
-	let selectedTab: 'teams' | 'pros' = 'pros'; // changed to 'pros'
+	let selectedTab: 'teams' | 'pros' = 'pros';
 	let teams: any[] = [];
 	let pros = [];
 	let isLoading = true;
 	let error: string | null = null;
 
-	onMount(async () => {
-		try {
-			// Fetch both teams and pros
-			const teamsResponse = await supabase.from('teams').select('*');
-			const prosResponse = await supabase.from('pros').select('*');
+onMount(async () => {
+	try {
+		const teamsResponse = await supabase.from('teams').select('*');
+		const prosResponse = await supabase.from('pros').select('*');
 
-			if (teamsResponse.error) throw teamsResponse.error;
-			if (prosResponse.error) throw prosResponse.error;
+		if (teamsResponse.error) throw teamsResponse.error;
+		if (prosResponse.error) throw prosResponse.error;
 
-			teams = teamsResponse.data;
-			pros = prosResponse.data.map((pro) => {
-				const associatedTeam = teams.find((team) => team.team_id === pro.team_id);
-				console.log('Pro:', pro.name, 'TeamID:', pro.team_id, 'Associated Team:', associatedTeam);
-				return {
-					...pro,
-					team: associatedTeam ? associatedTeam.name : 'No Team'
-				};
-			});
-			isLoading = false;
-		} catch (err) {
-			error = err.message;
-			isLoading = false;
-		}
-	});
+		teams = teamsResponse.data;
+		pros = prosResponse.data.map((pro) => {
+			const associatedTeam = teams.find((team) => team.team_id === pro.team_id);
+			return {
+				...pro,
+				team: associatedTeam ? associatedTeam.name : 'No Team',
+				team_image_url: associatedTeam ? associatedTeam.team_image_url : null
+			};
+		}).sort((a, b) => a.team.localeCompare(b.team));
+
+		isLoading = false;
+	} catch (err) {
+		error = err.message;
+		isLoading = false;
+	}
+});
+
 </script>
 
 <div class="p-6">
 	<h1 class="text-5xl sm:text-7xl md:text-9xl font-bold mb-4 uppercase">Standings</h1>
 
 	<div class="flex flex-wrap space-x-4 mb-4">
-		<!-- Pros button moved to the left -->
 		<button
 			class="flex-1 px-2 py-1 sm:px-4 sm:py-2 transition duration-300 ease-in-out hover:bg-blue-700 font-bold"
 			class:bg-blue-500={selectedTab === 'pros'}
@@ -50,7 +50,6 @@
 		>
 			Pros
 		</button>
-		<!-- Teams button -->
 		<button
 			class="flex-1 px-2 py-1 sm:px-4 sm:py-2 transition duration-300 ease-in-out hover:bg-blue-700 font-bold"
 			class:bg-blue-500={selectedTab === 'teams'}
@@ -73,13 +72,14 @@
 			{/each}
 		</div>
 	{:else}
-		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+		<div class="grid grid-cols-2 gap-4">
 			{#each pros as pro}
 				<ProCard
 					name={pro.name}
 					gender={pro.gender}
 					proImageUrl={pro.pro_image_url}
 					team={pro.team}
+					teamImageUrl={pro.team_image_url}
 					earnings={pro.earnings}
 					points={pro.points}
 				/>
