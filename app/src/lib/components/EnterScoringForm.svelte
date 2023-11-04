22e791d5e0009057b1bfbb76c3352d1176797221
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-11-04 19:07:39 +0000

    Teams and pros displaying start

--- app/src/lib/components/EnterScoringForm.svelte
+++ app/src/lib/components/EnterScoringForm.svelte
@@ -5,12 +5,41 @@
 	import Incrementer from '$lib/components/Incrementer.svelte';
 	import Decrementer from '$lib/components/Decrementer.svelte';
 	import Resetter from '$lib/components/Resetter.svelte';
+	import { getTeamColors } from '$lib/utilities/teamColors.ts';
 
 	let activeStep = 1;
 	let steps = [];
 	let startHole;
 	let currentScores = {}; // This will hold the current scores for the active hole
+	let pros = [];
+	let teams = [];
 	let scoresValue;
+
+	async function loadProsAndTeams(proIds, teamIds) {
+		// Load specific pros based on passed IDs
+		if (proIds && proIds.length) {
+			let prosQuery = `pro_id=in.(${proIds.join(',')})`;
+			let prosResponse = await supabase.from('pros').select('*').in('pro_id', proIds);
+
+			if (prosResponse.error) {
+				console.error('Error loading pros:', prosResponse.error);
+			} else {
+				pros = prosResponse.data;
+			}
+		}
+
+		// Load specific teams based on passed IDs
+		if (teamIds && teamIds.length) {
+			let teamsQuery = `id=in.(${teamIds.join(',')})`; // This creates a query string for multiple IDs
+			let teamsResponse = await supabase.from('teams').select('*').in('team_id', teamIds);
+
+			if (teamsResponse.error) {
+				console.error('Error loading teams:', teamsResponse.error);
+			} else {
+				teams = teamsResponse.data;
+			}
+		}
+	}
 
 	// Subscribe to the scores store
 	scores.subscribe((value) => {
@@ -34,7 +63,6 @@
 			startHole = scores[0].score_hole_start;
 
 			console.log(scores[0].detailed_scores);
-			console.log(scores[0].detailed_scores[1].det_sco_group_name);
 
 			if (scoresError) {
 				throw scoresError;
@@ -54,6 +82,20 @@
 				steps = Object.entries(detailedScores).map(([key, holeData]: [string, any]) => {
 					const groupName = holeData.det_sco_group_name;
 					console.log(groupName);
+					const par = holeData.det_sco_par;
+					console.log(par);
+					const female_a = holeData.det_sco_female_a;
+					console.log(female_a);
+					const male_a = holeData.det_sco_male_a;
+					console.log(male_a);
+					const female_b = holeData.det_sco_female_b;
+					console.log(female_b);
+					const male_b = holeData.det_sco_male_b;
+					console.log(male_b);
+					const team_a = holeData.det_sco_team_a_id;
+					console.log(team_a);
+					const team_b = holeData.det_sco_team_b_id;
+					console.log(team_b);
 					const holeNumber = holeData.det_sco_hole_number;
 					const isActiveHole = holeNumber === startHole; // Compare holeNumber with startHole
 					console.log(`Hole ${holeNumber} active: ${isActiveHole}`);
@@ -61,6 +103,13 @@
 						id: holeNumber,
 						hole: `Hole ${holeNumber}`,
 						group: groupName, // Add the group name to the step
+						par: par,
+						female_a: female_a,
+						male_a: male_a,
+						female_b: female_b,
+						male_b: male_b,
+						team_a: team_a,
+						team_b: team_b,
 						active: isActiveHole
 					};
 				});
@@ -70,8 +119,24 @@
 		}
 	}
 
-	onMount(() => {
-		fetchScoringData();
+	onMount(async () => {
+		await fetchScoringData(); // Assuming this populates the `steps` array.
+
+		// Now that we have the `steps` data, we can extract the necessary IDs.
+		const proIds = new Set();
+		const teamIds = new Set();
+
+		steps.forEach((step) => {
+			if (step.female_a) proIds.add(step.female_a);
+			if (step.male_a) proIds.add(step.male_a);
+			if (step.female_b) proIds.add(step.female_b);
+			if (step.male_b) proIds.add(step.male_b);
+			if (step.team_a) teamIds.add(step.team_a);
+			if (step.team_b) teamIds.add(step.team_b);
+		});
+
+		// Convert the Sets to arrays before passing them to loadProsAndTeams
+		await loadProsAndTeams([...proIds], [...teamIds]);
 	});
 </script>
 
@@ -79,7 +144,7 @@
 	<ol
 		class="flex flex-wrap gap-4 justify-center text-sm font-medium text-gray-500 dark:text-gray-400"
 	>
-		{#each steps as { id, hole, group, active }}
+		{#each steps as { id, hole, group, par, female_a, male_a, team_a, team_b, active }}
 			<li class="flex items-center">
 				<span class="flex items-center">
 					<svg
@@ -101,23 +166,50 @@
 		{/each}
 	</ol>
 	{#if steps.length > 0}
-		{#each steps as { id, hole, group, active }}
+		{#each steps as { id, hole, group, par, female_a, male_a, female_b, male_b, team_a, team_b, active }}
 			{#if active}
 				<form on:submit|preventDefault={submitScores}>
 					<fieldset>
-						<legend>{group}<br />{hole} - Par 4</legend>
-						{#each Object.keys(scoresValue) as pro}
-							<h1 class="mt-4 mb-4">Score for {pro}: {scoresValue[pro]}</h1>
-							<Incrementer {pro} />
-							<Decrementer {pro} />
-							<Resetter {pro} />
+						<legend>{group} <br />{hole} - Par {par}</legend>
+
+						<!-- Use pro_id to match the correct pro -->
+						<h1 class="mt-4 mb-4">
+							Female A Name: {pros.find((p) => p.pro_id === female_a)?.name || 'Unknown'}
+						</h1>
+						<h1 class="mt-4 mb-4">
+							Male A Name: {pros.find((p) => p.pro_id === male_a)?.name || 'Unknown'}
+						</h1>
+
+						<!-- Use pro_id to match the correct pro -->
+						<h1 class="mt-4 mb-4">
+							Female B Name: {pros.find((p) => p.pro_id === female_b)?.name || 'Unknown'}
+						</h1>
+						<h1 class="mt-4 mb-4">
+							Male B Name: {pros.find((p) => p.pro_id === male_b)?.name || 'Unknown'}
+						</h1>
+
+						<!-- Lookup team names by team ID, assuming team id property is 'id' -->
+						<h1 class="mt-4 mb-4">
+							Team A Name: {teams.find((t) => t.team_id === team_a)?.name || 'Unknown'}
+						</h1>
+						<h1 class="mt-4 mb-4">
+							Team B Name: {teams.find((t) => t.team_id === team_b)?.name || 'Unknown'}
+						</h1>
+
+						<!-- Iterate through each score and match pro names to their scores -->
+						{#each Object.keys(scoresValue) as pro_id}
+							<!-- Make sure to find the pro's name using pro_id -->
+							<h1 class="mt-4 mb-4">
+								Score for {pros.find((p) => p.pro_id.toString() === pro_id)?.name || 'Unknown'}: {scoresValue[
+									pro_id
+								]}
+							</h1>
+							<Incrementer {pro_id} />
+							<Decrementer {pro_id} />
+							<Resetter {pro_id} />
 						{/each}
-						<button
-							type="submit"
-							class="bg-gray-500 text-white font-bold py-2 px-4 rounded hover:bg-gray-600 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
-						>
-							Submit Scores
-						</button>
+
+						<button type="submit" class="...">Submit Scores</button>
 					</fieldset>
 				</form>
 			{/if}
