c2691ca50ad269a146d7829fe052b6bb3d91f2ba
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-11-21 15:20:35 +0000

    Zero ot scores when moving on

--- app/src/lib/components/EnterScoringForm.svelte
+++ app/src/lib/components/EnterScoringForm.svelte
@@ -299,6 +299,11 @@
 	}
 
 	async function submitStepUpdate(startHole: number) {
+		// Reset score values when moving to the next hole
+		$femaleA = 0;
+		$maleA = 0;
+		$femaleB = 0;
+		$maleB = 0;
 		let error = null;
 		try {
 			let currentHoleIndex = startHole - 1; // Convert to 0-based index
@@ -565,7 +570,7 @@
 							d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"
 						/>
 					</svg>
-					<span class={index === startHole ? 'active-hole' : ''}>{hole}</span>
+					<span class={index + 1 === startHole ? 'active-hole' : ''}>{hole}</span>
 				</span>
 			</li>
 		{/each}
