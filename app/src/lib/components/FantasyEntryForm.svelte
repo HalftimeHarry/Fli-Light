34ecf32d6ac200ce93d97ec4c52406af53a0b802
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-11-29 16:56:08 +0000

    update form

--- app/src/lib/components/FantasyEntryForm.svelte
+++ app/src/lib/components/FantasyEntryForm.svelte
@@ -16,6 +16,7 @@
 			type="text"
 			id="leagueName"
 			bind:value={leagueName}
+			placeholder="Example Name"
 			class="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
 		/>
 	</div>
