52634d4ba3514f77789d98eb47490b7a6700b312
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-11-30 01:48:32 +0000

    Working league and tournaments

--- app/src/lib/components/FantasyEntryForm.svelte
+++ app/src/lib/components/FantasyEntryForm.svelte
@@ -36,7 +36,7 @@
 		// Use the user's UUID
 		const userUUID = user.id;
 
-		// Create league
+		// Create league and retrieve the generated ID
 		const { data: leagueData, error: leagueError } = await supabase
 			.from('league')
 			.insert([{ league_name: leagueName, created_by: userUUID }])
@@ -47,11 +47,22 @@
 			return;
 		}
 
-		// Create fantasy tournaments
-		if (leagueData) {
-			await createFantasyTournaments(leagueData.league_id, leagueName);
+		// After the league is created, query to get its ID
+		const { data: createdLeague, error: fetchError } = await supabase
+			.from('league')
+			.select('league_id')
+			.eq('created_by', userUUID)
+			.order('created_at', { ascending: false })
+			.limit(1)
+			.single();
+
+		if (fetchError) {
+			console.error('Error fetching created league:', fetchError);
+		} else if (createdLeague) {
+			await createFantasyTournaments(createdLeague.league_id, leagueName);
 		}
 	};
+
 	async function createFantasyTournaments(leagueId, leagueName) {
 		for (let i = 1; i <= 12; i++) {
 			const tournamentName = `${leagueName} Tournament ${i}`;
@@ -73,21 +84,21 @@
 </script>
 
 <form on:submit|preventDefault={handleSubmit} class="space-y-4">
-    <div>
-        <label for="leagueName" class="block text-sm font-medium text-white">League Name:</label>
-        <input
-            type="text"
-            id="leagueName"
-            bind:value={leagueName}
-            placeholder="Example Name"
-            class="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-    </div>
+	<div>
+		<label for="leagueName" class="block text-sm font-medium text-white">League Name:</label>
+		<input
+			type="text"
+			id="leagueName"
+			bind:value={leagueName}
+			placeholder="Example Name"
+			class="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+		/>
+	</div>
 
-    <button
-        type="submit"
-        class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-    >
-        Create League
-    </button>
+	<button
+		type="submit"
+		class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+	>
+		Create League
+	</button>
 </form>
