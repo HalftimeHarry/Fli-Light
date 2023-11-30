c3f3e115ba4791ba6503774960b22c9a2f3673e9
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-11-30 00:00:42 +0000

    Working 1

--- app/src/lib/components/FantasyEntryForm.svelte
+++ app/src/lib/components/FantasyEntryForm.svelte
@@ -9,6 +9,7 @@
 		created_by: null // Assign the user's UUID here
 		// Other fields will use default values set in the database
 	};
+	let leagueName = '';
 
 	// Example initialization within your Svelte component script
 	let fantasyTournament: FantasyTournament = {
@@ -24,8 +25,6 @@
 	};
 
 	const handleSubmit = async () => {
-		// Example: use league.league_name in your API call or logic
-    	console.log('League name:', league.league_name);
 		// Retrieve the logged-in user's details
 		const { data: userData } = await supabase.auth.getUser();
 		const user = userData.user;
@@ -36,51 +35,59 @@
 
 		// Use the user's UUID
 		const userUUID = user.id;
-		console.log(userUUID);
 
-		const { data, error } = await supabase
+		// Create league
+		const { data: leagueData, error: leagueError } = await supabase
 			.from('league')
-			.insert([{ league_name: 'league.league_name', created_by: userUUID }])
-			.select();
+			.insert([{ league_name: leagueName, created_by: userUUID }])
+			.single();
 
-		if (error) {
-			console.error('Error creating league:', error);
+		if (leagueError) {
+			console.error('Error creating league:', leagueError);
 			return;
 		}
 
-		// Now call create_fantasy_tournaments function with the necessary arguments
-		if (data) {
-			let { error: tournamentError } = await supabase.rpc('create_fantasy_tournaments', {
-				league_id_arg: data, // Assuming leagueResult is the new league ID
-				league_name_arg: league.league_name,
-				tournaments_count: 12 // Or however many tournaments you want to create
-			});
-
-			if (tournamentError) {
-				console.error('Error creating fantasy tournaments:', tournamentError);
-			} else {
-				console.log('Fantasy tournaments created for league:', leagueResult);
-			}
+		// Create fantasy tournaments
+		if (leagueData) {
+			await createFantasyTournaments(leagueData.league_id, leagueName);
 		}
 	};
+	async function createFantasyTournaments(leagueId, leagueName) {
+		for (let i = 1; i <= 12; i++) {
+			const tournamentName = `${leagueName} Tournament ${i}`;
+			const { error } = await supabase.from('fantasy_tournaments').insert([
+				{
+					league_id: leagueId,
+					fantasy_tournament_name: tournamentName
+					// Add other fields like start_date, end_date, etc., as necessary
+				}
+			]);
+
+			if (error) {
+				console.error(`Error creating ${tournamentName}:`, error);
+			} else {
+				console.log(`${tournamentName} created successfully`);
+			}
+		}
+	}
 </script>
 
 <form on:submit|preventDefault={handleSubmit} class="space-y-4">
-	<div>
-		<label for="leagueName" class="block text-sm font-medium text-white">League Name:</label>
-		<input
-    type="text"
-    id="leagueName"
-    bind:value={league.league_name}
-    placeholder="Example Name"
-    class="..."
-/>
-	</div>
+    <div>
+        <label for="leagueName" class="block text-sm font-medium text-white">League Name:</label>
+        <input
+            type="text"
+            id="leagueName"
+            bind:value={leagueName}
+            placeholder="Example Name"
+            class="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+        />
+    </div>
 
-	<button
-		type="submit"
-		class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-	>
-		Create League
-	</button>
+    <button
+        type="submit"
+        class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+    >
+        Create League
+    </button>
 </form>
