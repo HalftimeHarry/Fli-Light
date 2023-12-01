5cc4e1560421eb2dfb636fba36a9bffa70a4af8c
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-12-01 04:24:19 +0000

    Displaying data in page

--- app/src/routes/league/[league_id]/+page.svelte
+++ app/src/routes/league/[league_id]/+page.svelte
@@ -1,34 +1,35 @@
 <script context="module">
-	import { supabase } from '/workspace/Fli-Light/app/src/supabaseClient.ts';
+    import { supabase } from '/workspace/Fli-Light/app/src/supabaseClient.ts';
 
-	let { data: league, error } = await supabase.from('league').select('*');
-	console.log(league);
-	let leagueId = league[0].league_id;
-	let leagueName = league[0].league_name;
-	let createdBy = league[0].created_by;
-	let draftStatus = league[0].draft_status;
-	let currentParticipantCount = league[0].current_participant_count.toString();
-	let crowdfundingProgress = league[0].crowdfunding_progress;
-	let entryFee = league[0].entry_fee;
-	let totalRounds = league[0].total_rounds;
-	let maxParticipants = league[0].max_participants;
-	let registrationDeadline = new Date(league[0].registration_deadline).toLocaleDateString();
-	let paymentModel = league[0].payment_model;
-	// Other properties can be added similarly
+    let { data: league, error } = await supabase.from('league').select('*');
+    console.log(league);
+    let leagueData = league[0];
+    let registrationDeadline = new Date(leagueData.registration_deadline).toLocaleDateString();
 </script>
 
 {#if error}
-	<p>Error loading league: {error}</p>
-{:else if leagueId}
-	<h1>League: {leagueName}</h1>
-	<p>Created by: {createdBy}</p>
-	<p>Draft Status: {draftStatus}</p>
-	<p>Current Participants: {currentParticipantCount}</p>
-	<p>Crowdfunding Progress: {crowdfundingProgress}</p>
-	<p>Entry Fee: ${entryFee}</p>
-	<p>Total Rounds: {totalRounds}</p>
-	<p>Max Participants: {maxParticipants}</p>
-	<p>Registration Deadline: {registrationDeadline}</p>
-	<p>Payment Model: {paymentModel}</p>
-	<!-- Add other properties here as needed -->
+    <p>Error loading league: {error}</p>
+{:else if leagueData}
+    <h1>League: {leagueData.league_name}</h1>
+    <p>Created by: {leagueData.created_by}</p>
+    <p>Draft Status: {leagueData.draft_status}</p>
+    <p>Current Participants: {leagueData.current_participant_count}</p>
+    <p>Entry Fee: ${leagueData.entry_fee}</p>
+    <p>Total Rounds: {leagueData.total_rounds}</p>
+    <p>Max Participants: {leagueData.max_participants}</p>
+    <p>Registration Deadline: {registrationDeadline}</p>
+    <p>Payment Model: {leagueData.payment_model}</p>
+
+    {#if leagueData.is_crowdfunded}
+        <p>Crowdfunding Progress: ${leagueData.crowdfunding_progress}</p>
+        <!-- Display additional crowdfunding details and options -->
+    {/if}
+
+    {#if leagueData.fantasy_tournament_active}
+        <!-- Display active fantasy tournament details -->
+    {/if}
+
+    {#if !leagueData.fantasy_team_count_met}
+        <!-- Display options for joining or forming teams -->
+    {/if}
 {/if}
