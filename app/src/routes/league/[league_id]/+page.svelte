0ce992b9f76affc931e24c5bf20ae97f3338b262
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-12-01 02:58:39 +0000

    Surfacing data

--- app/src/routes/league/[league_id]/+page.svelte
+++ app/src/routes/league/[league_id]/+page.svelte
@@ -1,26 +1,15 @@
-<script lang="ts">
-	import { onMount } from 'svelte';
-	import { page } from '$app/stores';
+<script context="module">
+	import { supabase } from '/workspace/Fli-Light/app/src/supabaseClient.ts';
 
-	let leagueId;
-
-	$: leagueId = $page.params.league_id;
-
-
-	let leagueData;
-
-	onMount(async () => {
-		// Fetch the league data based on leagueId
-		// Example: `fetchLeagueData(leagueId)` is a function you would define to fetch league data
-		leagueData = await fetchLeagueData(leagueId);
-	});
-
-	// Define the function to fetch league data
-	async function fetchLeagueData(id) {
-		// Implement the logic to fetch data from your API or database
-		// Example: return fetch(`/api/leagues/${id}`).then(res => res.json());
-	}
+	let { data: league, error } = await supabase.from('league').select('*');
+	console.log(league[0].league_id);
+	let leagueToUse = league[0].league_id;
+	let leagueName = league[0].league_name;
 </script>
 
-<h1>League: {leagueData?.name}</h1>
-<p>Details about the league will be displayed here.</p>
+{#if error}
+	<p>Error loading league: {error}</p>
+{:else if leagueToUse}
+	<h1>League: {leagueName}</h1>
+	<p>Details about the league will be displayed here.</p>
+{/if}
