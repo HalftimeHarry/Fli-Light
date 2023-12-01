b1b7c7f603406b8f6fa4c561dbb00a98290de88d
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-12-01 04:14:16 +0000

    Basic page data displaying

--- app/src/routes/league/[league_id]/+page.svelte
+++ app/src/routes/league/[league_id]/+page.svelte
@@ -2,14 +2,33 @@
 	import { supabase } from '/workspace/Fli-Light/app/src/supabaseClient.ts';
 
 	let { data: league, error } = await supabase.from('league').select('*');
-	console.log(league[0].league_id);
-	let leagueToUse = league[0].league_id;
+	console.log(league);
+	let leagueId = league[0].league_id;
 	let leagueName = league[0].league_name;
+	let createdBy = league[0].created_by;
+	let draftStatus = league[0].draft_status;
+	let currentParticipantCount = league[0].current_participant_count.toString();
+	let crowdfundingProgress = league[0].crowdfunding_progress;
+	let entryFee = league[0].entry_fee;
+	let totalRounds = league[0].total_rounds;
+	let maxParticipants = league[0].max_participants;
+	let registrationDeadline = new Date(league[0].registration_deadline).toLocaleDateString();
+	let paymentModel = league[0].payment_model;
+	// Other properties can be added similarly
 </script>
 
 {#if error}
 	<p>Error loading league: {error}</p>
-{:else if leagueToUse}
+{:else if leagueId}
 	<h1>League: {leagueName}</h1>
-	<p>Details about the league will be displayed here.</p>
+	<p>Created by: {createdBy}</p>
+	<p>Draft Status: {draftStatus}</p>
+	<p>Current Participants: {currentParticipantCount}</p>
+	<p>Crowdfunding Progress: {crowdfundingProgress}</p>
+	<p>Entry Fee: ${entryFee}</p>
+	<p>Total Rounds: {totalRounds}</p>
+	<p>Max Participants: {maxParticipants}</p>
+	<p>Registration Deadline: {registrationDeadline}</p>
+	<p>Payment Model: {paymentModel}</p>
+	<!-- Add other properties here as needed -->
 {/if}
