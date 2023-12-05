c4cc558ce62396e8490dfefd06773d7752f3fa40
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-12-05 21:39:43 +0000

    Fetching date and time

--- app/src/routes/league/[league_id]/+page.svelte
+++ app/src/routes/league/[league_id]/+page.svelte
@@ -7,11 +7,6 @@
 		isFantasyParticipantJoinLeaguePopupVisible.set(true);
 	}
 
-	let { data: league, error } = await supabase.from('league').select('*');
-	console.log(league);
-	let leagueData = league[0];
-	let userUUID; // UUID of the logged-in user
-
 	// Define participantFields at the module level
 	const participantFields = [
 		'league_participant_1',
@@ -21,6 +16,59 @@
 		'league_participant_5',
 		'league_participant_6'
 	];
+
+	async function fetchNextFantasyTournament(leagueId) {
+		let { data: fantasyTournaments, error } = await supabase
+			.from('fantasy_tournaments')
+			.select('start_date')
+			.eq('league_id', leagueId)
+			.order('start_date', { ascending: true })
+			.limit(1);
+
+		if (error) {
+			console.error('Error fetching fantasy tournament start date:', error);
+			return null;
+		} else if (fantasyTournaments && fantasyTournaments.length > 0) {
+			let firstTournamentStartDate = new Date(fantasyTournaments[0].start_date);
+			let draftStartTime = new Date(firstTournamentStartDate.getTime());
+			draftStartTime.setHours(draftStartTime.getHours() - 48); // 48 hours before the tournament start time
+			return draftStartTime;
+		} else {
+			console.log('No upcoming fantasy tournaments found');
+			return null;
+		}
+	}
+
+	let { data: league, error } = await supabase.from('league').select('*');
+	console.log(league);
+	let leagueData = league[0];
+	let userUUID; // UUID of the logged-in user
+
+	// Calculate the number of non-null participants
+	let nonNullParticipantCount = countNonNullParticipants(leagueData);
+	let needed = nonNullParticipantCount - 6;
+	let positiveValue = Math.abs(needed);
+	// Get the logged-in user's UUID
+	const { data: userData } = await supabase.auth.getUser();
+	userUUID = userData.user?.id;
+</script>
+
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		let { data: league, fetchError } = await supabase.from('league').select('*');
+		error = fetchError;
+		let draftStartTime;
+		let leagueIdForCountdown;
+		if (!fetchError && league && league.length > 0) {
+			leagueData = league[0];
+			leagueIdForCountdown = leagueData.league_id;
+			userUUID = (await supabase.auth.getUser()).data.user?.id;
+			draftStartTime = await fetchNextFantasyTournament(leagueIdForCountdown);
+			console.log(draftStartTime);
+		}
+	});
 
 	function countNonNullParticipants(leagueData) {
 		return participantFields.reduce(
@@ -53,16 +101,6 @@
 		}
 	}
 
-	// Calculate the number of non-null participants
-	let nonNullParticipantCount = countNonNullParticipants(leagueData);
-	let needed = nonNullParticipantCount - 6;
-	let positiveValue = Math.abs(needed);
-	// Get the logged-in user's UUID
-	const { data: userData } = await supabase.auth.getUser();
-	userUUID = userData.user?.id;
-</script>
-
-<script>
 	// Calculate the number of non-null participants
 	$: nonNullParticipantCount = leagueData ? countNonNullParticipants(leagueData) : 0;
 	$: additionalParticipantsNeeded = leagueData.max_participants - nonNullParticipantCount;
